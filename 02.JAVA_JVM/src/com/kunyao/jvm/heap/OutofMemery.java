279b57954a9707489ac3651635794c7e4fcb4a25
shikunyaoa <44891725+shikunyaoa@users.noreply.github.com>
2020-04-09 21:44:49 +0800

    @shikunaaa
    add jvm

---
+++ 02.JAVA_JVM/src/com/kunyao/jvm/heap/OutofMemery.java
@@ -0,0 +1,31 @@
+package com.kunyao.jvm.heap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName: OutofMemery
+ * @Author: kunyao
+ * @Description: 堆内存溢出异常   -Xms指定堆的大小
+ * @Date: 2020/4/9 21:27
+ * @Version: 1.0
+ */
+public class OutofMemery {
+
+    public static void main(String[] args) {
+        int i = 0;
+
+        try {
+            List<String> list = new ArrayList<>();
+            String str = "hello";
+            while(true){
+                list.add(str);
+                str = str + str;
+                i++;
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();  //java.lang.OutOfMemoryError: Java heap space
+            System.out.println(i); //26
+        }
+    }
+}
---
+++ 02.JAVA_JVM/src/com/kunyao/jvm/stack/stackoverflow.java
@@ -0,0 +1,28 @@
+package com.kunyao.jvm.stack;
+
+/**
+ * @ClassName: stackoverflow
+ * @Author: kunyao
+ * @Description: 栈内存溢出 -Xss指定栈的大小
+ * @Date: 2020/4/9 20:48
+ * @Version: 1.0
+ */
+public class stackoverflow {
+
+    public static int count = 0;
+
+    public static void main(String[] args) {
+
+        try {
+            countNumber();
+        } catch (Throwable e) {
+            e.printStackTrace();  //java.lang.StackOverflowError
+            System.out.println(count); //23709
+        }
+    }
+
+    public static  void countNumber(){
+        count++;
+        countNumber();
+    }
+}
