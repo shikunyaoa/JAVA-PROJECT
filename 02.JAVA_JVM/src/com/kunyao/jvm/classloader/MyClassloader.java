0fe7b6a9ca517b47f9788e6d83159e22bcc9c0fa
shikunyaoa <44891725+shikunyaoa@users.noreply.github.com>
2020-04-15 21:59:43 +0800

    @shikunaaa
    add classloader

---
+++ 02.JAVA_JVM/src/com/kunyao/jvm/classloader/MyClassloader.java
@@ -0,0 +1,37 @@
+package com.kunyao.jvm.classloader;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * @ClassName: MyClassloader
+ * @Author: kunyao
+ * @Description: 自定义类加载器
+ * @Date: 2020/4/15 21:00
+ * @Version: 1.0
+ */
+public class MyClassloader extends ClassLoader {
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        //class文件路径
+        String path = "";
+
+        try {
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            //将字节码读取到输入流
+            Files.copy(Paths.get(path), os);
+            //得到字节数组
+            byte[] bytes = os.toByteArray();
+
+            //byte -> *.class
+            return defineClass(name, bytes, 0, bytes.length);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw  new ClassNotFoundException("未找到字节码文件");
+        }
+    }
+}
