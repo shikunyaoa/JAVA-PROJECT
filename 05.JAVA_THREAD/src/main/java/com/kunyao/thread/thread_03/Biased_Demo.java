835dd742fa8e5b8c15d77a403d4279dc59d94d16
shikunyaoa <44891725+shikunyaoa@users.noreply.github.com>
2020-05-08 22:58:17 +0800

    @shikunaaa
    add Biased

---
+++ 05.JAVA_THREAD/src/main/java/com/kunyao/thread/thread_03/Biased_Demo.java
@@ -0,0 +1,28 @@
+package com.kunyao.thread.thread_03;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @ClassName: Biased_Demo
+ * @Author: kunyao
+ * @Description: 偏向锁测试
+ * @Date: 2020/5/8 22:21
+ * @Version: 1.0
+ */
+
+@Slf4j(topic = "c.Biased_Demo")
+public class Biased_Demo {
+
+    public static void main(String[] args) {
+
+       Dog d = new Dog();
+
+
+
+    }
+}
+
+
+class Dog{
+
+}
