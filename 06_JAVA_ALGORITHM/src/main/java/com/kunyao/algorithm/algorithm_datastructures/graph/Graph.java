597276f4e4e3ab1899e01a84177825c6a07b6120
shikunyaoa <44891725+shikunyaoa@users.noreply.github.com>
2020-06-26 21:27:03 +0800

    Update Graph.java

--- 06_JAVA_ALGORITHM/src/main/java/com/kunyao/algorithm/algorithm_datastructures/graph/Graph.java
+++ 06_JAVA_ALGORITHM/src/main/java/com/kunyao/algorithm/algorithm_datastructures/graph/Graph.java
@@ -2,6 +2,7 @@ package com.kunyao.algorithm.algorithm_datastructures.graph;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 /**
  * @ClassName: Graph
@@ -21,8 +22,11 @@ public class Graph {
     //表示边的数目
     private int numOfEdges;
 
+    //定义数组boolean[],记录某个节点是否被访问
+    private boolean[] isVisited = new boolean[5];
 
     public static void main(String[] args) {
+
 
     }
 
@@ -33,6 +37,126 @@ public class Graph {
         numOfEdges = 0;
     }
 
+    /**
+     * 得到一个邻接节点的下标
+     * @param index
+     * @return
+     */
+    public int getFirstNeighbor(int index){
+        for (int i = 0; i < vertexList.size() ; i++) {
+            if(edges[index][i] > 0){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * 根据前一个邻接节点的下标来获取下一个邻接节点
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public int getNextNeighbor(int v1, int v2){
+        for(int j = v2 + 1; j < vertexList.size(); j++ ){
+            if(edges[v1][j] > 0){
+                return j;
+            }
+        }
+        return -1;
+    }
+
+
+    /**
+     * 深度优先遍历算法（DFS)
+     * @param
+     */
+    public void dfs(boolean[] isVisited, int i){
+
+        //首先访问该节点
+        System.out.println(getValueByIndex(i) + "->");
+        //将节点设置为已经访问
+        isVisited[i] = true;
+        //获取第一个邻接节点
+        int w = getFirstNeighbor(i);
+        while(w != -1){
+            //没有被访问
+            if(!isVisited[w]){
+                //递归访问邻接节点
+                dfs(isVisited, w);
+            }
+            //如果节点已经被访问
+            w = getNextNeighbor(i, w);
+        }
+    }
+
+    /**
+     * 对dfs进行一个重载，并进行dfs
+     */
+    public void dfs(){
+        //遍历所有的节点，进行dfs
+        for (int i = 0; i < getNumOfVertex(); i++) {
+            if(!isVisited[i]){
+                dfs(isVisited, i);
+            }
+        }    
+    }
+
+
+    /**
+     *  对一个节点进行广度优先算法
+     */
+    private void bfs(boolean[] isVisited, int i){
+
+        //表示队列头结点的下标
+        int u;
+        //邻节点w
+        int w;
+        //队列，记录节点访问的顺序
+        LinkedList queue = new LinkedList();
+
+        //访问节点，输出节点信息
+        System.out.println(getValueByIndex(i) + "=>");
+        //标记为已访问
+        isVisited[i] = true;
+        //将节点加入到队列
+        queue.addLast(i);
+
+        while(!queue.isEmpty()){
+
+            //取出队列的头结点下标
+            u = (int) queue.getFirst();
+            //得到第一个节点的邻节点
+            w = getFirstNeighbor(u);
+
+            while(w != -1){
+                //是否访问过
+                if(!isVisited[w]){
+                    System.out.println(getValueByIndex(w) + "=>");
+
+                    //标记为已访问
+                    isVisited[w] = true;
+                    //入队
+                    queue.addLast(w);
+                }
+                //以u为前驱节点，找到w的下一个邻节点
+                w = getNextNeighbor(u, w);
+            }
+        }
+    }
+
+    /**
+     * 遍历所有的节点，都进行广度优先搜索
+     * @param
+     */
+    public void bfs(){
+        for (int i = 0; i < getNumOfVertex(); i++) {
+            if(!isVisited[i]){
+                bfs(isVisited, i);
+            }
+        }
+    }
 
     //插入节点
     public void insertVertex(String vertex){
