8e7bc74b0522420f49d55a28e33d78387e032d8b
sky <2439677061.com>
2021-01-13 16:41:49 +0800

    dddd

---
+++ 01.JAVA-BASE/src/com/kunyao/java/collection/Collection_example.java
@@ -0,0 +1,15 @@
+package com.kunyao.java.collection;
+
+import java.util.Collection;
+
+/**
+ * @author sky
+ * @description: 刨析容器顶级父类
+ * @date: 2021-01-13
+ */
+public class Collection_example {
+
+    public static void main(String[] args) {
+        //Collection();
+    }
+}
